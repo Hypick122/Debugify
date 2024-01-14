eb7c060ed1571e09828033711df697d902577809
Hypick122 <twerka420@gmail.com>
2024-01-15 00:02:02 +0300

    rename Patch -> Pathes

--- Debugify/Patches/PlayerControllerBPatch.cs
+++ Debugify/Patches/PlayerControllerBPatch.cs
@@ -6,8 +6,15 @@ namespace Debugify.Patch
     // [HarmonyPatch(typeof(PlayerControllerBPatch))]
     internal class PlayerControllerBPatch
     {
-    //     [HarmonyPatch("SwitchToItemSlot")]
-    //     [HarmonyPostfix]
+        [HarmonyPatch("SwitchToItemSlot")]
+        [HarmonyPostfix]
         static void SwitchToItemSlot(PlayerControllerB __instance) { }
+
+        [HarmonyPatch("ScrollMouse_performed")]
+        [HarmonyPostfix]
+        static int ScrollMouse_performed(bool forward)
+        {
+
+        }
     }
 }
